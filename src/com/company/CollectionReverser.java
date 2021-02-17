3f32c69665a54b658aaee439f2cab17539d5063f
robomajor <robokajor@gmail.com>
2021-02-17 22:15:51 +0100

    Third commit

--- src/com/company/CollectionReverser.java
+++ src/com/company/CollectionReverser.java
@@ -9,7 +9,7 @@ public class CollectionReverser {
     public void reverseCollection(String[] text) {
         ArrayList<String> words = new ArrayList<>(Arrays.asList(text));
         System.out.println("Collection before changes - " + words);
-        Collections.reverse(words);        //odwracanie kolekcji
+        Collections.reverse(words);
         System.out.println("Collection after changes - " + words);
         System.out.println();
     }
